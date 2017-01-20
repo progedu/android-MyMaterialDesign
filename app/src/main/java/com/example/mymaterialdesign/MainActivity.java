6d5c1e75ea0b604fd0b8c9bc858fb1b174b5552f
Soichiro-Yoshimura <yoshimura@soichiro.org>
2017-01-20 20:50:29 +0900

    Add RecyclerView

--- app/src/main/java/com/example/mymaterialdesign/MainActivity.java
+++ app/src/main/java/com/example/mymaterialdesign/MainActivity.java
@@ -2,12 +2,48 @@ package com.example.mymaterialdesign;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SimpleItemAnimator;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private List<String> listItem = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        for (int i = 0; i < 100 ; i++) {
+            listItem.add("Item" + Integer.toString(i));
+        }
+        mAdapter = new MyAdapter(listItem);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    public void addItem(View view) {
+        listItem.add(0, "Added Item" + System.currentTimeMillis());
+        mAdapter.notifyItemRangeChanged(0, listItem.size());
     }
 }
